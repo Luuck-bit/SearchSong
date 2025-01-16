4081bd7afdd8049a267ecb1761dd0cd4d643d24f
Luuck-bit <joaohdn4546@gmail.com>
2025-01-16 14:23:18 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+const clientId = `8e0d46f1890a40ebac8dbbe80bbce99c`;
+const clientSecret = `9c2e0b5412bf4a47ab4a4e7de8336c28`
+const encodedCredentials = btoa(`${clientId}:${clientSecret}`);
+
+const informs = document.getElementById('informs');
+
+function requestSong(){
+  const searchValue = serchBar.value;
+  async function getAccessToken() {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+      body: 'grant_type=client_credentials',
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to fetch access token');
+    }
+  
+    const data = await response.json();
+    return data.access_token; // Token de acesso
+  }
+
+
+  async function searchTracks(query) {
+    const token = await getAccessToken();
+  
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error('Failed to fetch tracks');
+    }
+  
+    const data = await response.json();
+    return data.tracks.items; // Retorna as músicas encontradas
+  }
+  
+  // Exemplo de uso
+  searchTracks(searchValue).then(tracks => {
+    tracks.forEach(track => {
+      console.log(`Música: ${track.name}, Artista: ${track.artists[0].name}`);
+      informs.innerHTML += `
+      <div>
+      <div id="data">
+        <img src="${track.album.images[1].url}" alt="Album Cover">
+        <div>
+          <h1>${track.name}</h1>
+          <p><strong>Artist:</strong> ${track.artists[0].name}</p>
+          <p><strong>Album:</strong> ${track.album.name}</p>
+          <audio controls>
+          <source src="${track.preview_url}" type="audio/mpeg">
+          Your browser does not support the audio element.
+          </audio>
+          <a href="${track.external_urls.spotify}" target="_blank"><img src="./icons/spotify.png" alt="Listen on Spotify"><p>Listen on Spotify</p></a>
+        </div>
+      </div>
+      <hr>
+      </div>
+      `
+    });
+  });
+
+}
+
+const serchBar = document.getElementById('search');
+const button = document.getElementById('button');
+
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  requestSong();
+})
+serchBar.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    requestSong();
+  }
+})
+
+
+
+
+
+
